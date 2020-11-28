00cb9a3a31c9b0f40423012e6ee4803aa6039650
Allen Liu <allen6qi@gmail.com>
2020-11-28 20:58:29 +0800

    conneted to DB

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,6 +9,15 @@ router.get('/chatroom', (req, res, next)=>{
     res.render('chatroom')
 })
 
+router.get('./chat', (req, res, next)=>{
+    res.render('rooms')
+})
+
+router.get('*', (req, res)=>{
+    res.status(404).render('404')
+})
+
+
 module.exports = {
     router
 }
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 'use strict'
 
 const express = require('express');
-const app = express();
 const chatApp = require('./app/routes')
+const connectDB = require('./app/db')
+require('dotenv').config()
 
+const app = express();
 app.set("port", process.env.PORT || 3000)
+connectDB();
+
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
